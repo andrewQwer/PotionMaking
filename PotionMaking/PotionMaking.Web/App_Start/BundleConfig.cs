65ddc61c2d9b83f73b9afb35b62fd13038c2e703
akaravaychik <akaravaychik@servicechannel.com>
2015-07-09 14:44:22 +0300

    folder structure changed

---
+++ PotionMaking/PotionMaking.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PotionMaking.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/Vendor/jquery/dist/jquery.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/Vendor/bootstrap/dist/bootstrap.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Scripts/Vendor/bootstrap/dist/css/bootstrap.css"));
+        }
+    }
+}
