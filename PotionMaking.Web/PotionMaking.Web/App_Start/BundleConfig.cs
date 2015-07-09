a04758b5dd8d371ab9582f787df55bfb652d7337
akaravaychik <akaravaychik@servicechannel.com>
2015-07-09 14:38:04 +0300

    added jquery and bootstrap

--- PotionMaking.Web/PotionMaking.Web/App_Start/BundleConfig.cs
+++ PotionMaking.Web/PotionMaking.Web/App_Start/BundleConfig.cs
@@ -8,7 +8,13 @@ namespace PotionMaking.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-           
+            bundles.Add(new ScriptBundle("~/Content/main").Include(
+                "~/Scripts/Vendor/jquery/dist/jquery.js",
+                "~/Scripts/Vendor/bootstrap/dist/bootstrap.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Scripts/Vendor/bootstrap/dist/css/bootstrap.css"));
         }
     }
 }
