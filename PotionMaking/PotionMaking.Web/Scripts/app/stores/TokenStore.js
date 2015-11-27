7f96a8d3d34ba4a406838071f84816cbb26b6fe8
akaravaychik <akaravaychik@servicechannel.com>
2015-11-27 17:18:26 +0300

    added constant for local storage key

--- PotionMaking/PotionMaking.Web/Scripts/app/stores/TokenStore.js
+++ PotionMaking/PotionMaking.Web/Scripts/app/stores/TokenStore.js
@@ -4,6 +4,7 @@ var rand = function () {
     return Math.random().toString(36).substr(2); // remove `0.`
 };
 var _registrationToken = null;
+var AUTH_TOKEN_KEY = 'pm-auth-token';
 
 var tokenStore = {
     generateToken: function () {
@@ -23,11 +24,11 @@ var tokenStore = {
     setAuthToken: function(token) {
         if (token) {
             token.created = new Date();
-            localStorage.setItem('token', JSON.stringify(token));
+            localStorage.setItem(AUTH_TOKEN_KEY, JSON.stringify(token));
         }
     },
     getAuthToken: function(){
-        var token = JSON.parse(localStorage.getItem('token'));
+        var token = JSON.parse(localStorage.getItem(AUTH_TOKEN_KEY));
         return token;
     }
 };
